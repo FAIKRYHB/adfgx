261f13f3933f8e025c97c6725a4835621182f340
Jan Hošek <j_hosek@utb.cz>
2018-11-10 23:45:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 class adfgvx():
+    tablecord=[]
     lang="W"
+    def TableSize(self,bit): #Checkbox
+        if bit:
+            self.tablecord=["AA","AD","AF","AG","AX","DA","DD","DF","DG","DX","FA","FD","FF","FG","FX","GA","GD","GF","GG","GX","XA","XD","XF","XG","XX"]
+        else:
+            self.tablecord=["AA","AD","AF","AG","AV","AX","DA","DD","DF","DG","DV","DX","FA","FD","FF","FG","FV","FX","GA","GD","GF","GG","GV","GX","VA","VD","VF","VG","VV","VX","XA","XD","XF","XG","XV","XX"]
+    
     def CookRawString(self,rawstring):
             cookedstring=""
             reple = [["a","á","ä","Á","Ä"],["b"],["č","ć","c","Č","Ć"],["d","ď","Ď"],["ě","é","e","ë","Ě","É","Ë"],["f"],["g"],["h"],["i","í","ï","Í","Ï"],["j"],["k"],["l","ĺ","Ĺ"],["m"],["n","ň","ń","Ň","Ń"],["o","ó","ö","Ó","Ö"],["p"],["q"],["r","ř","ŕ","Ř","Ŕ"],["š","ś","s","Š","Ś"],["t","ť","Ť"],["u","ú","ů","Ů","Ú"],["v"],["w"],["x"],["y","ý","ÿ","Ý","Ÿ"],["z","ž","ź","Ž","Ź"]]
@@ -9,7 +16,7 @@ class adfgvx():
                     if rawstring[i]==" ":
                         cookedstring+="MEZERA"
                     else:
-                        if rawstring[i]==chr(ord(self.lang)+32):
+                        if self.tablecord==25 and rawstring[i]==chr(ord(self.lang)+32):
                             if self.lang=="W":
                                 cookedstring+="V"
                             if self.lang=="J":
@@ -19,7 +26,7 @@ class adfgvx():
                                 if rawstring[i] in reple[j]:
                                     cookedstring+=forThing[j]
                 else:
-                    if rawstring[i]==self.lang:
+                    if self.tablecord==25 and rawstring[i]==self.lang:
                         if self.lang=="W":
                             cookedstring+="V"
                         if self.lang=="J":
@@ -29,18 +36,16 @@ class adfgvx():
             return cookedstring
         
     
-    def Substituce(self,cookedstring,table):
+    def Sifrovat(self,cookedstring,key,table):
+        #substituce
         burnedstring=""
-        tablecord=["AA","AD","AF","AG","AX","DA","DD","DF","DG","DX","FA","FD","FF","FG","FX","GA","GD","GF","GG","GX","XA","XD","XF","XG","XX"]
-        tablecordV=["AA","AD","AF","AG","AV","AX","DA","DD","DF","DG","DV","DX","FA","FD","FF","FG","FV","FX","GA","GD","GF","GG","GV","GX","VA","VD","VF","VG","VV","VX","XA","XD","XF","XG","XV","XX"]
         for i in range(0,len(cookedstring)):
             for j in range(0,len(table)):
                 if cookedstring[i]==table[j]:
-                    burnedstring+=tablecord[j]
+                    burnedstring+=self.tablecord[j]
                     break
-        return burnedstring
-    
-    def Transpozice(self,key,burnedstring):
+        #print(burnedstring)
+        #transpozice
         x=len(key)
         keyL=[]
         sloupce=[]
@@ -52,14 +57,42 @@ class adfgvx():
             for j in range(0,x):
                 if i%x==j:
                     sloupce[j]+=burnedstring[i]
+        #print(sloupce)
         for i in range(1,x):
             for j in range(0,i):
                 if keyL[i]<keyL[j]:
                     keyL.insert(j,keyL.pop(i))
                     sloupce.insert(j,sloupce.pop(i))
-        return [sloupce,keyL]
+        burnedstring=""
+        for i in range(0,len(sloupce)-1):
+            for j in range(0,len(sloupce[i])):
+                burnedstring+=sloupce[i][j]
+            burnedstring+=" "
+        for j in range(0,len(sloupce[-1])):
+                burnedstring+=sloupce[-1][j]
+        #return [sloupce,keyL]
+        #print(sloupce)
+        return burnedstring
     
-    def DeTranspozice(self,key,keyL,sloupce):
+    def Desifrovat(self,instring,key,table):
+        x=len(key)
+        sloupce=[]
+        for i in range(0,x):
+            sloupce.append([])
+        len(testoutdva)
+        j=0
+        for i in range(x):
+            while instring[j]!=" ":
+                sloupce[i].append(instring[j])
+                j+=1
+                if j==len(instring):
+                    break
+            j+=1
+        keyL=[]
+        for i in range(0,x):
+            keyL.append(key[i])
+        keyL.sort()
+        #transpozice
         x=len(key)
         for i in range(0,x):
             for j in range(i,x):
@@ -67,21 +100,24 @@ class adfgvx():
                     keyL.insert(i,keyL.pop(j))
                     sloupce.insert(i,sloupce.pop(j))
                     break
-        outstring=""
+        string=""
         for i in range(0,len(sloupce[0])):
             while len(sloupce[-1])==0:
                 sloupce.pop()
             for j in range(0,len(sloupce)):
-                outstring+=sloupce[j].pop(0)
+                string+=sloupce[j].pop(0)
+        #print(string)
+        #substituce
+        outstring=""
+        for i in range(0,len(string),2):
+            outstring+=table[self.tablecord.index(string[i]+string[i+1])]
         return outstring
 
 ####
 testin="ahojpepojaksemas"
 testinstance=adfgvx()
+testinstance.TableSize(True)
 testout=testinstance.CookRawString(testin)
 testtable="ERHPQZBGFOAMLNYXSDITVCKJU"
-testoutdva=testinstance.Substituce(testout,testtable)
-testouttri=testinstance.Transpozice("KOLOTOC",testoutdva)
-testkey=testouttri[1]
-testsloupce=testouttri[0]
-DEtest=testinstance.DeTranspozice("KOLOTOC",testkey,testsloupce)
+testoutdva=testinstance.Sifrovat(testout,"KOLOTOC",testtable)
+Detest=testinstance.Desifrovat(testoutdva,"KOLOTOC",testtable)
